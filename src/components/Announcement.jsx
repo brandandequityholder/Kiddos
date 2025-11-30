9a4ea2226eb0f3caf5740dc010dc61ba90f4733b
frontendmoe <frontendmoe@gmail.com>
2025-12-01 00:19:23 +0100

    Fixes

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -4,11 +4,11 @@ function Announcement() {
 			<div className='fixed z-[90] top-0 w-full h-[78px] overflow-hidden flex justify-center items-center bg-[#F8EA2B]'>
 				<div className='flex w-full justify-center space-x-[5px] lg:space-x-[10px] items-center'>
 					<p className="text-right  MadeGentle justify-start text-blue-800 text-[10px] lg:text-lg font-normal font-['MADE_Gentle'] leading-[50.33px] tracking-wide">
-						DAY CAMP STARTS IN 293 DAYS
+						CAMP STARTS JUNE 8, 2026
 					</p>
 					<Link>
 						<p className="text-right underline  MadeGentle justify-start text-blue-800 text-[10px] lg:text-lg  font-normal font-['MADE_Gentle'] leading-[50.33px] tracking-wide">
-							CLICK HERE TO ENROLL TODAY!
+							CLICK HERE TO ENROLL NOW
 						</p>
 					</Link>
 				</div>
